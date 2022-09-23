d89d07d39c8e4b56ce7b04091a74d5f4d681c499
Mani Jafari <contact@manijafari.com>
2022-09-23 16:19:27 -0700

    Change the titleLabel type on WelcomeViewController to CLTypingLabel as well as on Main storyboard file

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,23 +7,26 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        titleLabel.text = ""
-        var characterIndex = 0
-        let titleText = "⚡️FlashChat"
-        for letter in titleText {
-            Timer.scheduledTimer(withTimeInterval: 0.1 * Double(characterIndex), repeats: false) { timer in
-                self.titleLabel.text?.append(letter)
-            }
-            characterIndex += 1
-        }
+//        titleLabel.text = ""
+//        var characterIndex = 0
+//        let titleText = "⚡️FlashChat"
+//        for letter in titleText {
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * Double(characterIndex), repeats: false) { timer in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            characterIndex += 1
+//        }
+        titleLabel.text = "⚡️FlashChat"
+
     }
     
 
